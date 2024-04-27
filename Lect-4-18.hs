7de98902de946b41a8002e6e1b8cc5299d8841a3
DakotaT21 <drtompki@ucsc.edu>
2024-04-26 18:25:36 -0700

    notes 4-18

--- Lect-4-18.hs
+++ Lect-4-18.hs
@@ -1,157 +1,112 @@
-message :: String
-message = "Welcome to lecture 7!"
-
 -- Agenda:
-
--- Haskell features
--- - Tuples
--- - Product types, sum types, and recursive types
-
--- PL concepts
--- - More practice with recursion (really a *programming* concept)
--- - Tail recursion
--- - Abstract syntax trees and doing stuff with them
-
--- With tuples, the length is part of the type (unlike with lists!)
-
-database :: [(String, Integer, String)]
-database = [("Lindsey", 42, "bulbasaur"), ("Alex", 41, "mew"), ("Sylvia", 6, "houndoom")]
-
-lookupAge :: [(String, Integer, String)] -> String -> Integer 
-lookupAge []             _    = error "That person isn't in the database!"
-lookupAge ((n,a,_):tups) name = if n == name then a else lookupAge tups name
-
-bogusDatabase :: [(String, Integer, String)]
-bogusDatabase = [("30450934354", 893290842309432, "Lindsey")]
-
--- It seems that built-in types don't capture all the info we want.
--- Let's switch to defining our own types!
-
--- A product type puts different things together (similar to a tuple)
--- Here "DBRecord" is a *constructor* for things of type
--- NameAgePokemonDatabaseRecord.
-data NameAgePokemonDatabaseRecord = DBRecord HouseholdPerson Integer String
-
-database' :: [NameAgePokemonDatabaseRecord]
-database' = [DBRecord Lindsey 42 "bulbasaur",
-             DBRecord Alex 41 "mew",
-             DBRecord Sylvia 6 "houndoom"]
-
-lookupAge' :: [NameAgePokemonDatabaseRecord] -> HouseholdPerson -> Integer 
-lookupAge' []                    _    = error "That person isn't in the database!"
-lookupAge' (DBRecord n a _:recs) name = if n == name then a else lookupAge' recs name
-
--- Sum types are for giving alternatives.
-data HouseholdPerson = Lindsey | Alex | Sylvia
-  deriving Eq
-
--- Let's talk about a data structure for arithmetic expressions.
--- 3 + 3, 5 - 2, 3 * 5, etc.
--- But we also want stuff like
--- 3 + (4 - 2)
--- ((2 + 8) - (10 * 6)) - 1
-
-data SillyArithExpr = SillyPlus Int Int | SillyMinus Int Int | SillyTimes Int Int
-
-threePlusThree :: SillyArithExpr
-threePlusThree = SillyPlus 3 3
-
-fiveMinusTwo :: SillyArithExpr
-fiveMinusTwo = SillyMinus 5 2
-
-threeTimesFive :: SillyArithExpr
-threeTimesFive = SillyTimes 3 5
-
--- This won't scale to arithmetic expressions of arbitrary size.
--- So, recursive types to the rescue!
-data ArithExpr = Plus ArithExpr ArithExpr 
-               | Minus ArithExpr ArithExpr
-               | Times ArithExpr ArithExpr
-               | Leaf Int -- The base case
--- 3 + (4 - 2)
-bigExpression :: ArithExpr
-bigExpression = Plus (Leaf 3) (Minus (Leaf 4) (Leaf 2))
-
--- ((2 + 8) - (10 * 6)) - 1
-biggerExpression :: ArithExpr
-biggerExpression = Minus (Minus (Plus (Leaf 2) (Leaf 8)) 
-                                (Times (Leaf 10) (Leaf 6))) (Leaf 1)
-
--- I was just a human parser.
--- I took "((2 + 8) - (10 * 6)) - 1", a string,
--- and I parsed it into an ArithExpr.
--- We've turned it into an Abstract Syntax Tree (AST).
--- And the ArithExpr type combines these concepts we just learned,
--- sum types, product types, and recursive types.
+    -- Lists 
+        -- List Constructors
+        -- PAttern matching on lists
+        -- Writing list literals
+        -- List append
+        -- Strings are also lists
+        -- Polymorphic list types
+    -- Guards in function definitions
+    -- Using 'div' and 'mod'
+    -- Using infix functions
 
 
--- A type for binary trees with ints stored only at the leaves
-data Tree = JustLeaf Int | NodeTree Tree Tree
+-- Lists are either 
+    --empty: []
+    --have a head *element*, attached to a tail *list*: 
+
+        -- ["pikachu"]
+        -- Head "pikachu", tail []
+
+        -- ["pikachu", "mew", "bulbasuar"]
+        -- Head "pikachu", tail ["mew", "bulbasuar"]
+    
+    -- Were always going to have to deal with 
+        --empty list case
+        --case where list has head and tail
+
+    --Every list is constructed out of either:
+        -- empty list constructor []
+        -- the : constructor, pronounced "cons" which puts a head and tail together
 
 
--- Tail recursion
+-- :: means "has type"
+-- [a] is a type variable, takes list of int string etc instead of being specific
+-- ourLength should return type Int
 
--- Recall this function:
--- Takes a number n and returns the sum of n + n-1 + n-2 + ... + 1 + 0.
-ourSum :: Int -> Int
-ourSum 0 = 0
-ourSum n = n + ourSum (n - 1)
+ourLength :: [a] -> Int
+--empty list case
+ourLength []     = 0
+--head/tail case, head element (not used so _ not x) bound to the tail list, recursive
+ourLength (_:xs) = 1 + ourLength xs
 
--- How does it work?
--- This is (roughly) a picture of what happens when we execute `ourSum 5`
--- ourSum 5
--- 5 + ourSum 4
--- 5 + (4 + ourSum 3)
--- 5 + (4 + (3 + ourSum 2))
--- 5 + (4 + (3 + (2 + ourSum 1)))
--- 5 + (4 + (3 + (2 + (1 + ourSum 0))))
--- 5 + (4 + (3 + (2 + (1 + 0))))
--- 5 + (4 + (3 + (2 + 1)))
--- 5 + (4 + (3 + 3))
--- 5 + (4 + 6)
--- 5 + 10
--- 15
-
--- HUGE CAVEAT: Haskell is what's called a "lazy" language.
--- This means that things don't actually always evaluate in the order you expect.
--- We'll talk more about this later.
-
--- Let's try rewriting ourSum in a way that uses the stack more sparingly.
--- We're going to use another argument and treat it as an *accumulator*.
-ourSum' :: Int -> Int -> Int
-ourSum' 0 acc = acc
-ourSum' n acc = ourSum' (n-1) (acc+n)
-
--- ourSum' 5 0
--- ourSum' 4 5 -- we've accumulated 5 by now
--- ourSum' 3 9 -- we've accumulated 5 + 4 by now
--- ourSum' 2 12 -- we've accumulated 5 + 4 + 3 by now
--- ourSum' 1 14
--- ourSum' 0 15
--- 15
-
--- This is called *tail-recursive* code.
--- There's nothing around the outside of the recursive call
--- that's *waiting* for it to finish.
- 
+--Built-in 'head' function, this is what it looks like
+ourHead :: [a] -> a
+ourHead []    = error "Empty"
+ourHead (x:_) = x
 
 
+-- Better to use pattern matching instad of 'head' and 'tail', safer, better style
+appendHellotoAll :: [String] -> [String]
+appendHellotoAll []     = []
+appendHellotoAll (x:xs) = (x ++ "hello") : appendHellotoAll xs
 
 
+-- Strings are just lists of Char
+removeFirstChar :: String -> String
+removeFirstChar []     = error "Empty"
+removeFirstChar (_:xs) = xs
+-- This code is redundant with 'tail'
 
 
+-- :t tells you the type of something
+    -- ':t apple' --- "apple" :: String
+    -- ':t (++)' --- (+_+) :: [a] -> [a] -> [a]
 
 
+-- Append
+-- can be infix or prefix
+    -- (++) "apple" "orange"
+    -- "apple" ++ "orange"
+
+-- Function that appends "hello" to only the second element of the list
+appendSecond :: [String] -> [String]
+-- empty list case
+appendSecond []       = []
+-- one element list case
+appendSecond [x]   = [x]
+-- everything else case
+appendSecond (x:y:ys) = x : (y ++ "hello") : ys
+-- x first elem, y second elem, ys list
 
 
+-- Guards
+lessThanFive :: Int -> String
+-- Patten match, | is a guard, after | is a boolean expression evaluating for =
+lessThanFive n | n < 5  = "Hooray"
+lessThanFive n | n >= 5 ="Boo"
+
+compareNum :: Int -> Int -> String
+compareNum n m | n == m = "Hooray"
+compareNum n m | n /= m = "Boo"
+
+-- Uses "otherwise" as a catch all, always evaluates to true
+compareNum' :: Int -> Int -> String
+compareNum' n m | n == m = "Hooray"
+                | otherwise = "Boo"
 
 
+-- Div and Mod
+    -- div 4000 10 = 400
+    -- 4000 'div' 10 = 400
+    -- mod 4321 100 = 21
+    -- 4321 'mod' 100 = 21
 
+-- Equals
+    -- 5 == 6 = False
+    -- (==) 5 6 = False
+    -- "apple" == "apple" = True
+    -- [1, 2, 3] == [1, 2, 3] = True
 
-
-
-
-
-
-
-
+    -- Doesnt work on functions
+    -- (\x -> x) == (\x -> x) = Error
